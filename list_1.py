f180ca8bc1ce92f4a02c0d8b0f648687e967c1e2
aparnabreddy <aparnabreddy26@gmail.com>
2021-07-21 07:31:03 +0530

    list operaations performed

---
+++ list_1.py
@@ -0,0 +1,10 @@
+cities=["Bangalore","Hyderabad","Delhi","Mangalore"]
+towns=["pavagada","chitradurga"]
+print(cities) #printing list elements
+print(cities+towns) #merging two lists
+print(len(cities)) #finding length of list
+print(len(cities+towns))
+cities.remove('Mangalore') #removinh element from list
+print(cities)
+cities.pop(2)
+print(cities)
