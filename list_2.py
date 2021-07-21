412001434a44fcb8b3ecf594b36a5254170ca4b1
aparnabreddy <aparnabreddy26@gmail.com>
2021-07-21 07:44:12 +0530

    list operaations performed

---
+++ list_2.py
@@ -0,0 +1,13 @@
+movies=["Hello","KGF","Uppena","radhe shyam"]
+Actors=["Allu Arjun","Prabhas","Yash","Akshay Kumar","salman khan"]
+print(movies)
+print(movies+Actors)
+print(len(movies))
+print(len(movies+Actors))
+movies.remove('Hello')
+print(movies)
+del movies[1]
+print(movies)
+movies.clear()
+print(movies)
+
